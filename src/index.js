23df376703c45288212789322e1906608f6ef789
dcyprien <dcyprien@42.student.fr>
2025-09-26 10:12:29 +0200

    index shop mockup done

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import data from '../data/data.json' with { type: "json" };
+const productlist = document.getElementById("products");
+var len = data.length;
+console.log(data[0])
+console.log('hello');
