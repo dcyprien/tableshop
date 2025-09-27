8f2f76444bfeb8b77b0fdc68e74541e79a3c2a04
dcyprien <dcyprien@42.student.fr>
2025-09-27 16:39:37 +0200

    full complete, need to polish the about us page and comment the classes

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import data from '../data/data.json' with { type: "json" };
-const productlist = document.getElementById("products");
-var len = data.length;
-console.log(data[0])
-console.log('hello');
